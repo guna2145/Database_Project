43818a96b066706db3be0eaef029139372945771
Guna <gunasekar108@live.com>
2023-01-08 00:00:53 +0530

    Latest Push

--- createdb.py
+++ createdb.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-SQLAlchemy(app)
+SQLAlchemy['SQLALCHEMY_DATABASE_URI'] = '/home/logan/Documents/Database_Project'
 
+
+SQLAlchemy(app)
+app.config(SQLAlchemy)
